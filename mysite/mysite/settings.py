1a339f909e7794c504224c31be69e52a66f91d2b
Andrew L Hnilica <ahnilica@gmail.com>
2016-02-28 22:11:14 -0500

    Undid bad sql settings for testing purposes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,6 +77,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'jkohann_sp16',
         'USER': 'jkohann',
