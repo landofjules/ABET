69ee2d90466f5b62a34b721c61ba31950b37e069
Julian Kohann <jkohann@gmail.com>
2016-03-03 23:25:01 +0000

    fixed views and settings

--- mysite/ABET_DB/views.py
+++ mysite/ABET_DB/views.py
@@ -6,19 +6,15 @@ from ABET_DB.models import studentOutcomes
 from django.http import HttpResponse
 
 def index(request):
-<<<<<<< HEAD
-    return HttpResponse("ABET_DB HOME PAGE")
+    template = loader.get_template('ABET_DB/index.html')
+    return HttpResponse(template.render())
     
 def showData(request):
     outcomeList = studentOutcomes.objects.order_by('outcomeLetter')
     output = ', '.join([q.outcomeLetter for q in outcomeList])
     return HttpResponse(output)
-=======
-    template = loader.get_template('polls/index.html')
-    return HttpResponse(template.render())
     
 # one view w 3 boxes:
 #   StudentOutcomes : outcomeLetters, descripeion
 #   PreformanceLevels : aceivementLevel, description
 #   Courses : CRNnumber, CourseName, course_description
->>>>>>> 7f1936debf1170f4d0384b0ebdc90efe6675965c
