1160aaadf8c25711929b53ae257660bed5a3fb7e
ahnilica <ahnilica@gmail.com>
2016-03-04 00:26:49 +0000

    Added better formating

--- mysite/ABET_DB/views.py
+++ mysite/ABET_DB/views.py
@@ -12,25 +12,25 @@ def showData(request):
     outcomeList = studentOutcomes.objects.order_by('outcomeLetter')
     
     if len(outcomeList) == 0:
-        str1 = '&nbsp;&nbsp;&nbsp;NONE<br>'
+        str1 = '&nbsp;&nbsp;&nbsp;NONE<br><br>'
     else:
-        str1 = '&nbsp;&nbsp;&nbsp;' + ', '.join([q.outcomeLetter for q in outcomeList]) + '<br>'
+        str1 = '&nbsp;&nbsp;&nbsp;' + ', '.join([q.outcomeLetter for q in outcomeList]) + '<br><br>'
     
     title2 = 'courses: courseName<br><br>'
     outcomeList = courses.objects.order_by('courseName')
     
     if len(outcomeList) == 0:
-        str2 = '&nbsp;&nbsp;&nbsp;NONE<br>'
+        str2 = '&nbsp;&nbsp;&nbsp;NONE<br><br>'
     else:
-        str2 = '&nbsp;&nbsp;&nbsp;' + ', '.join([q.courseName for q in outcomeList]) + '<br>'
+        str2 = '&nbsp;&nbsp;&nbsp;' + ', '.join([q.courseName for q in outcomeList]) + '<br><br>'
         
-    title3 = 'performanceLevels: achievementLevel<br>'
+    title3 = 'performanceLevels: achievementLevel<br><br>'
     outcomeList = performanceLevels.objects.order_by('achievementLevel')
     
     if len(outcomeList) == 0:
         str3 = '&nbsp;&nbsp;&nbsp;NONE<br><br>'
     else:
-        str3 = '&nbsp;&nbsp;&nbsp;' + ', '.join([str(q.achievementLevel) for q in outcomeList]) + '<br>'
+        str3 = '&nbsp;&nbsp;&nbsp;' + ', '.join([str(q.achievementLevel) for q in outcomeList]) + '<br><br>'
     
     output = title1 + str1 + title2+ str2 + title3 + str3
     return HttpResponse(output)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,14 +77,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-<<<<<<< Updated upstream
-=======
 
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
->>>>>>> Stashed changes
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
         #'ENGINE': 'django.db.backends.mysql',
         #'NAME': 'jkohann_sp16',
