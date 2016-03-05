5c3940ce7deb2cb51e420099635dcf87307a1f67
ahnilica <ahnilica@gmail.com>
2016-03-05 23:03:16 +0000

    Saving database changes to allow merge

--- mysite/ABET_DB/admin.py
+++ mysite/ABET_DB/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from ABET_DB.models import performanceLevels, performanceIndicators
 from ABET_DB.models import studentOutcomes, courseOutcomeMap, rubrics
-from ABET_DB.models import outcomeData, piData
+from ABET_DB.models import outcomeData, piData, courses
 
 admin.site.register(performanceLevels)
 admin.site.register(performanceIndicators)
@@ -12,3 +12,4 @@ admin.site.register(studentOutcomes)
 admin.site.register(courseOutcomeMap)
 admin.site.register(rubrics)
 admin.site.register(piData)
+admin.site.register(courses)
